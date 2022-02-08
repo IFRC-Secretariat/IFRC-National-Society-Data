c58be5c5f545e0be270ee427e6404a0c824f1ce3
Alex Howes <howesalexandra@gmail.com>
2022-02-08 13:01:45 +0100

    Move the renaming of columns out of the reload_data method and into the indicators method of dataset.

--- src/ns_contacts/ns_contacts_dataset.py
+++ src/ns_contacts/ns_contacts_dataset.py
@@ -28,16 +28,10 @@ class NSContactsDataset(Dataset):
         """
         Read in data from the NS Databank API and save to file, or read in as a CSV file from the given filepath.
         """
-        # Pull data from FDRS API and save the data locally
+        # Pull data from FDRS API
         response = requests.get(url=f'https://data-api.ifrc.org/api/entities/ns?apiKey={self.api_key}')
         response.raise_for_status()
-
-        # Convert to a pandas DataFrame and rename columns for consistency with other datasets
         data = pd.DataFrame(response.json())
-        data.rename(columns={'KPI_DON_code': 'National Society ID',
-                             'NSO_DON_name': 'National Society name',
-                             'NSO_ZON_name': 'Region name',
-                             'ZON_Code': 'Region code'}, errors='raise', inplace=True)
 
         # Save the data
         data.to_csv(self.filepath, index=False)
@@ -47,9 +41,7 @@ class NSContactsDataset(Dataset):
         """
         Transform and process the data, including changing the structure and selecting columns.
         """
-        # Convert NS IDs to NS names
-        self.data['National Society name'] = DatabankNSIDMapper(api_key=self.api_key).map(self.data['National Society ID'])
-
         # Make sure the NS names agree with the central list
+        self.data.rename(columns={'NSO_DON_name': 'National Society name'}, errors='raise', inplace=True)
         self.data['National Society name'] = NSNamesCleaner().clean(self.data['National Society name'])
         self.data.set_index('National Society name', inplace=True)
