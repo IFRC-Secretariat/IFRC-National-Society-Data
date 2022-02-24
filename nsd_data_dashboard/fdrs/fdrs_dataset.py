4d2a0d5d22850cbe304b16836f656af52327c1dd
Alex Howes <howesalexandra@gmail.com>
2022-02-24 22:28:09 +0100

    Add processing to get the top three income sources, and change boolean columns so false values are dropped. Reorder columns.

--- nsd_data_dashboard/fdrs/fdrs_dataset.py
+++ nsd_data_dashboard/fdrs/fdrs_dataset.py
@@ -57,6 +57,11 @@ class FDRSDataset(Dataset):
         # Make sure the NS names agree with the central list
         self.data['National Society name'] = NSNamesCleaner().clean(self.data['National Society name'])
 
+        # Replace False for nan for boolean indicators, so that they are dropped in the next step
+        get_latest_columns = ['KPI_hasFinancialStatement', 'audited', 'ar', 'sp']
+        self.data.loc[(self.data['indicator'].isin(get_latest_columns)) & (self.data['value'].astype(str)=='False'), 'value'] = float('nan')
+        self.data.loc[(self.data['indicator'].isin(get_latest_columns)) & (self.data['value'].astype(str)=='True'), 'value'] = 'Yes'
+
         # Keep only the latest values for each indicator: keep the smallest value if there are duplicates
         self.data = self.data.dropna(how='any')\
                              .sort_values(by=['year', 'value'], ascending=[False, True])\
@@ -69,4 +74,37 @@ class FDRSDataset(Dataset):
                                     columns='indicator',
                                     values=['value', 'year', 'source'])\
                              .swaplevel(axis='columns')\
+                             .sort_index(axis='columns', level=0, sort_remaining=False)
+
+        # Calculate the top income sources
+        def get_top_n_income_sources(row, n=3):
+            total_income_column = 'KPI_IncomeLC'
+            income_source_columns = {'h_gov': 'Home government', 'f_gov': 'Foreign government', 'ind': 'Individuals', 'corp': 'Corporations', 'found': 'Foundations', 'un': 'UN agencies & other multilateral agencies', 'pooled_f': 'Pooled funds', 'ngo': 'Non-governmental organizations', 'si': 'Service income', 'iga': 'Income-generating activity', 'KPI_incomeFromNSsLC': 'Other National Society', 'ifrc': 'IFRC', 'icrc': 'ICRC', 'other': 'Other source'}
+            if row['KPI_IncomeLC', 'value'] != row['KPI_IncomeLC', 'value']:
+                if row['KPI_IncomeLC_CHF', 'value'] != row['KPI_IncomeLC_CHF', 'value']:
+                    return
+                income_source_columns = {source+'_CHF': alt for source, alt in income_source_columns.items()}
+                total_income_column += '_CHF'
+            source_year = row[total_income_column, 'year']
+            for column in income_source_columns:
+                if row[column, 'year']!=source_year:
+                    return row
+            total_income = float(row[total_income_column, 'value'])
+            income_amounts = row[[[column, 'value'] for column in income_source_columns]].astype(float).dropna().sort_values(ascending=False)
+            if not income_amounts.empty:
+                for i in range(0, n):
+                    row[f'income_source_{i+1}', 'value'] = f'{income_source_columns[income_amounts.index[i][0]]} ({round(100*income_amounts[i]/total_income)}%)'
+                    row[f'income_source_{i+1}', 'source'] = 'FDRS'
+                    row[f'income_source_{i+1}', 'year'] = source_year
+            return row
+
+        self.data = self.data.apply(lambda row: get_top_n_income_sources(row), axis=1)
+
+        # Order the data columns
+        def order_columns(x):
+            order = ['value', 'year', 'source']
+            order_map = {item: order.index(item) for item in order}
+            return x.map(order_map)
+
+        self.data = self.data.sort_index(axis='columns', level=1, key=lambda x: order_columns(x))\
                              .sort_index(axis='columns', level=0, sort_remaining=False)
