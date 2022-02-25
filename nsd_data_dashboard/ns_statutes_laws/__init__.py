f2c1a35105201bf02336b7e5072e47b19dff3645
Alex Howes <howesalexandra@gmail.com>
2022-02-25 09:43:58 +0100

    Add a module to read in and process data on NS recognition laws.

--- nsd_data_dashboard/ns_statutes_laws/__init__.py
+++ nsd_data_dashboard/ns_statutes_laws/__init__.py
@@ -1,1 +1,2 @@
 from nsd_data_dashboard.ns_statutes_laws.ns_statutes import NSStatutesDataset
+from nsd_data_dashboard.ns_statutes_laws.ns_recognition_laws import NSRecognitionLawsDataset
---
+++ nsd_data_dashboard/ns_statutes_laws/ns_recognition_laws.py
@@ -0,0 +1,42 @@
+"""
+Module to handle NS Recognition Laws data, including loading it from the data file, cleaning, and processing.
+"""
+import re
+import pandas as pd
+from nsd_data_dashboard.common import Dataset
+from nsd_data_dashboard.common.cleaners import NSNamesCleaner
+
+
+class NSRecognitionLawsDataset(Dataset):
+    """
+    Load NS Recognition Laws data from the file, and clean and process the data.
+    The filepath should be the location of the NS Recognition Laws data.
+
+    Parameters
+    ----------
+    filepath : string (required)
+        Path to save the dataset when loaded, and to read the dataset from.
+    """
+    def __init__(self, filepath, indicators=None):
+        super().__init__(filepath=filepath, reload=False, indicators=indicators)
+        pass
+
+
+    def process(self):
+        """
+        Transform and process the data, including changing the structure and selecting columns.
+        """
+        # Set the columns from the data row
+        self.data.columns = self.data.iloc[0]
+        self.data = self.data.iloc[1:]
+
+        # Clean up the column names
+        self.data.rename(columns={column: column.strip() for column in self.data.columns}, inplace=True)
+        self.data.rename(columns={'National Society (NS)': 'National Society name', 'Law/other regulations in force': 'Laws/ other regulations in force'}, inplace=True, errors='raise')
+
+        # Check that the NS names are consistent with the centralised names list
+        self.data['National Society name'] = NSNamesCleaner().clean(self.data['National Society name'].str.strip())
+
+        # Add another column level
+        self.data = self.data.set_index(['National Society name'])
+        self.data.columns = pd.MultiIndex.from_product([self.data.columns, ['']])
